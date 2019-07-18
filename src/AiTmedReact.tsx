1b9340cd15cee51cf7afd7286c66c16da89dcec5
pfftdammitchris <pfftdammitchris@gmail.com>
2019-07-18 09:47:38 -0700

    Added custom theme prop for callers

--- src/AiTmedReact.tsx
+++ src/AiTmedReact.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './config/theme'
 
-const AiTmedReact: React.FC<React.ReactNode> = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+interface LibraryProps {
+  theme: any
+}
+
+const AiTmedReact: React.FC<LibraryProps> = ({ children, ...props }) => (
+  <ThemeProvider theme={props.theme || theme}>{children}</ThemeProvider>
 )
 
 export default AiTmedReact
