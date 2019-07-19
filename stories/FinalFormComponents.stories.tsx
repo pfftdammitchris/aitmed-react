c7344af7bb6723d1a2947f1f391fc2d4ed4cb8c6
pfftdammitchris <pfftdammitchris@gmail.com>
2019-07-18 17:07:42 -0700

    G

--- stories/FinalFormComponents.stories.tsx
+++ stories/FinalFormComponents.stories.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 // import Typography from '@material-ui/core/Typography'
 import { storiesOf } from '@storybook/react'
+import { Form, Field } from 'react-final-form'
+import PhoneNumberField from '../../drafts/finalForm/fields/PhoneNumberField'
+
+const onSubmit = (values) => {
+  console.log(values)
+}
 
 storiesOf('react-final-form components', module).add('PhoneNumberField', () => (
-  <h3>dfsf</h3>
+  <Form
+    onSubmit={onSubmit}
+    render={({ handleSubmit, submitting }) => <form></form>}
+  />
 ))
