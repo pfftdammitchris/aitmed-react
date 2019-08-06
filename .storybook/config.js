dc18d4c1fecc596039a453c9d069e63a536ae4e0
pfftdammitchris <pfftdammitchris@gmail.com>
2019-08-06 16:10:38 -0700

    Added finalForm.ts util

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
+import { addParameters, configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { muiTheme } from 'storybook-addon-material-ui'
 import theme from '../src/config/theme'
 
 const req = require.context('../stories', true, /.stories.tsx$/)
 
-addDecorator(muiTheme(theme))
+addParameters({
+  options: {
+    showPanel: false,
+  },
+})
+
+addDecorator((p) => {
+  console.log(p())
+  return muiTheme([theme])(p)
+})
 addDecorator(
   withInfo({
     inline: true,
