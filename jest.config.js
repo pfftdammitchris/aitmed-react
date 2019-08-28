3d8a2fafd72fdb10c8faca42532943d2d5d5cb4e
pfftdammitchris <pfftdammitchris@gmail.com>
2019-08-27 17:04:41 -0700

    edited jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testPathIgnorePatterns: ['/lib/', '/node_modules/'],
+  testURL: 'http://127.0.0.1',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testURL: 'http://127.0.0.1',
   verbose: true,
 }
