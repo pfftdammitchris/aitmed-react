a913c01098157a9638f6f312bfe1060654c0b3f9
pfftdammitchris <pfftdammitchris@gmail.com>
2019-08-27 18:12:46 -0700

    added context to AiTmedReact wrapper

--- src/AiTmedReact.tsx
+++ src/AiTmedReact.tsx
@@ -3,11 +3,28 @@ import { ThemeProvider } from '@material-ui/styles'
 import theme from './config/theme'
 
 interface LibraryProps {
-  theme: any
+  theme?: any
+  env?: 'testapi' | 'api'
 }
 
-const AiTmedReact: React.FC<LibraryProps> = ({ children, ...props }) => (
-  <ThemeProvider theme={props.theme || theme}>{children}</ThemeProvider>
-)
+export interface ContextState {
+  env?: 'testapi' | 'api'
+  baseUrl: string
+}
+
+export const AiTmedContext = React.createContext<ContextState>({
+  env: undefined,
+  baseUrl: '',
+})
+
+const AiTmedReact: React.FC<LibraryProps> = ({ env, children, ...props }) => {
+  const baseUrl = `https://${env === 'testapi' ? `testapi.` : ''}aitmed.com`
+
+  return (
+    <AiTmedContext.Provider value={{ env, baseUrl }}>
+      <ThemeProvider theme={props.theme || theme}>{children}</ThemeProvider>
+    </AiTmedContext.Provider>
+  )
+}
 
 export default AiTmedReact
