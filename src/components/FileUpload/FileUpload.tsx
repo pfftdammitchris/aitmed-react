173ade59cfa986e71d0937032b8e4ae2819bacd5
Christopher Tran <pfftdammitchris@gmail.com>
2019-12-11 11:33:22 -0800

    changes to FileUpload export

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactProgressed from 'react-progressed'
-
-interface FileUploadProps {
-  children?: React.ReactNode
-  text?: string
-  onChange: (fileList: FileList, e: any) => void
-  inputRef?: {
-    readonly current: any | null
-  }
-}
-
-function FileUpload(props: FileUploadProps) {
-  return <ReactProgressed {...props} />
-}
-
-export default FileUpload
+export { default } from 'react-progressed'
