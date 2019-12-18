67a48ca3531fc9785d4cf8ccb0eab0506f684dd8
Christopher Tran <pfftdammitchris@gmail.com>
2019-12-18 10:32:27 -0800

    test

--- src/components/AlignOutlinedTextFields/index.tsx
+++ src/components/AlignOutlinedTextFields/index.tsx
@@ -1,3 +1,6 @@
-export { default } from './AlignOutlinedTextFields'
+export {
+  default,
+  AlignOutlinedTextFieldsProps,
+} from './AlignOutlinedTextFields'
 export { default as Context } from './Context'
 export { default as Provider } from './Provider'
